6f2ff57ccb4cd108f690521d092f28b0057385d1
milenanedeljkovic <80540793+milenanedeljkovic@users.noreply.github.com>
2023-02-03 16:14:51 +0100

    script 1st attempt

--- code/script.py
+++ code/script.py
@@ -3,28 +3,23 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import sys
 import spacy_conll
 import spacy_stanza
-from spacy_conll import init_parser
 import torch
 from transformers import AutoModel, AutoTokenizer
-from function_definitions import txt_to_conll, wiki_parsing, wiki_negated_clause_stats, get_contextual_embeddings
-from datasets import load_dataset
+from function_definitions import txt_to_conll, get_contextual_embeddings
+
+dataset = sys.argv[1]  # the dataset which we will analyse
 
 
-
-nlp = init_parser("en", "stanza", parser_opts={"use_gpu": True, "verbose": False}, include_headers=True)
+nlp = spacy_conll.init_parser("en", "stanza", parser_opts={"use_gpu": True, "verbose": False}, include_headers=True)
 tokenizer = AutoTokenizer.from_pretrained("roberta-base")
-model = AutoModel.from_pretrained('roberta-base')
+model = AutoModel.from_pretrained("roberta-base")
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 model.to(device)
 
-
-
-data = load_dataset("bigscience-data/roots-en-wikipedia", use_auth_token=True)
-
 verb_embeddings: Dict[str, [List[torch.Tensor], List[torch.Tensor]]] = {}
 
-for page in data:
+for page in dataset[:1]:
     page_text = page['text']
     with open("current_page.conll", "w") as file:
         file.write(txt_to_conll(page_text, nlp))
