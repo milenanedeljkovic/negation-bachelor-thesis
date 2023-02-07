4f161dda6bb6f260ca11441b3ce799167f7fe115
milenanedeljkovic <80540793+milenanedeljkovic@users.noreply.github.com>
2023-02-07 15:26:21 +0100

    testing

--- code/extract-phrases.py
+++ code/extract-phrases.py
@@ -5,8 +5,11 @@ from datasets import load_dataset
 
 dataset = load_dataset("bigscience-data/roots_en_wikipedia", use_auth_token=True)
 
+print("here")
+
 with open("negation-phrases", "a") as file:
     for page in dataset['train']:
+        print("a page")
         phrases = sent_tokenize(page['text'].lower())
         for phrase in phrases:
             if any([s in phrase for s in ["not", "never", "no", "cannot", "n't"]]):
