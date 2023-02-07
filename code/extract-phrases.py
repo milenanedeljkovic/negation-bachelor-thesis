653235e3c3509976f57f7cf6bbe8a8f15eef6082
milenanedeljkovic <80540793+milenanedeljkovic@users.noreply.github.com>
2023-02-07 16:14:42 +0100

    working version: extracting phrases with negation and their percentage

--- code/extract-phrases.py
+++ code/extract-phrases.py
@@ -6,13 +6,17 @@ from datasets import load_dataset
 dataset = load_dataset("bigscience-data/roots_en_wikipedia", use_auth_token=True)
 
 i = 0
-with open("negation-phrases", "a") as file:
+j = 0
+with open("phrases-with-negation.txt", "a") as file:
     for page in dataset['train']:
-        print(i)
-        i = i + 1
-        phrases = sent_tokenize(page['text'].lower())
+        phrases = sent_tokenize(page['text'])
         for phrase in phrases:
-            if any([s in phrase for s in ["not", "never", "no", "cannot", "n't"]]):
-                file.write(phrase)
+            j += 1
+            if any([s in phrase.lower() for s in ["not", "never", "no", "cannot", "n't", "no more"]]):
+                i += 1
+                file.write(f"{phrase}\n")
 
+
+with open("stats.txt", "a") as file:
+    file.write(f"Number of phrases with negation: {i} / {j} ({i / j}%)\n")
 
