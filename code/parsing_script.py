a9e6bdf60fc8beb8358a4b0d85af283310f46a87
milenanedeljkovic <80540793+milenanedeljkovic@users.noreply.github.com>
2023-02-08 17:43:33 +0100

    cuda?

--- code/parsing_script.py
+++ code/parsing_script.py
@@ -6,8 +6,9 @@ import sys
 from datetime import datetime
 import torch
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+#device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
 def txt_to_conll(text: str, nlp):
     """Input:
     - text: the string we want to parse
