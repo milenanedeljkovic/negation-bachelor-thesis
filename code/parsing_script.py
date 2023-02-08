620c052c976029b257e74f1a148d0849c03af80a
milenanedeljkovic <80540793+milenanedeljkovic@users.noreply.github.com>
2023-02-08 14:56:12 +0100

    torch grad

--- code/parsing_script.py
+++ code/parsing_script.py
@@ -23,43 +23,43 @@ def txt_to_conll(text: str, nlp):
     doc = nlp(text)
     return doc._.conll_str
 
-last_to_parse = int(sys.argv[1])
-
-dataset = load_dataset("bigscience-data/roots_en_wikipedia", use_auth_token=True)
-
-if last_to_parse >= len(dataset['train']):
-    raise ValueError(f"Cannot parse up to index {last_to_parse}, the last index of the dataset is {len(dataset['train']) - 1}.")
-
-with open("last_page_processed.txt", "r") as file:
-    # the first line in this file contains the index of the last processed page in the corpus
-    last_parsed = int(file.readline())
-
-if last_to_parse <= last_parsed:
-    raise ValueError(f"Already parsed up to index {last_parsed}.")
-
 with torch.no_grad():
+    last_to_parse = int(sys.argv[1])
+
+    dataset = load_dataset("bigscience-data/roots_en_wikipedia", use_auth_token=True)
+
+    if last_to_parse >= len(dataset['train']):
+        raise ValueError(f"Cannot parse up to index {last_to_parse}, the last index of the dataset is {len(dataset['train']) - 1}.")
+
+    with open("last_page_processed.txt", "r") as file:
+        # the first line in this file contains the index of the last processed page in the corpus
+        last_parsed = int(file.readline())
+
+    if last_to_parse <= last_parsed:
+        raise ValueError(f"Already parsed up to index {last_parsed}.")
+
     nlp = spacy_conll.init_parser("en", "stanza", parser_opts={"use_gpu": True, "verbose": False}, include_headers=True)
 
-now = datetime.now()
+    now = datetime.now()
 
-current_time = now.strftime("%H:%M:%S")
-print("Parsing Start Time =", current_time)
+    current_time = now.strftime("%H:%M:%S")
+    print("Parsing Start Time =", current_time)
 
-try:
-    for i in range(last_parsed + 1, last_to_parse + 1):
-        page_text = dataset['train'][i]['text']
-        with open("dependency_trees.conll", "a") as file:
-            file.write(txt_to_conll(page_text, nlp))
-except:
-    raise RuntimeError("Parsing failed.")
+    try:
+        for i in range(last_parsed + 1, last_to_parse + 1):
+            page_text = dataset['train'][i]['text']
+            with open("dependency_trees.conll", "a") as file:
+                file.write(txt_to_conll(page_text, nlp))
+    except:
+        raise RuntimeError("Parsing failed.")
 
 
-now = datetime.now()
+    now = datetime.now()
 
-current_time = now.strftime("%H:%M:%S")
-print("Parsing End Time =", current_time)
-print(f"Number of pages processed: {last_to_parse - last_parsed}")
+    current_time = now.strftime("%H:%M:%S")
+    print("Parsing End Time =", current_time)
+    print(f"Number of pages processed: {last_to_parse - last_parsed}")
 
-with open("last_page_processed.txt", "w") as file:
-    file.write(last_to_parse)
+    with open("last_page_processed.txt", "w") as file:
+        file.write(last_to_parse)
 
