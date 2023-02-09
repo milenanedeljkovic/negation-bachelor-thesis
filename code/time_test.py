8bf8389c85bae745713737271ccd08c408411732
milenanedeljkovic <80540793+milenanedeljkovic@users.noreply.github.com>
2023-02-09 11:33:56 +0100

    bug fixes

--- code/time_test.py
+++ code/time_test.py
@@ -8,8 +8,8 @@ import timeit
 nlp1 = init_parser("en", "stanza", parser_opts={"use_gpu": True, "verbose": False}, include_headers=True)
 nlp2 = stanza.Pipeline('en', processors='tokenize,pos', use_gpu=True, pos_batch_size=3000)
 
-t1 = timeit.timeit("nlp1(I like making clothes, but right now I am out of sewing thread.)", 20)
-t2 = timeit.timeit("nlp2(I like making clothes, but right now I am out of sewing thread.)", 20)
+t1 = timeit.timeit("nlp1(I like making clothes, but right now I am out of sewing thread.)", number=20)
+t2 = timeit.timeit("nlp2(I like making clothes, but right now I am out of sewing thread.)", number=20)
 
 print(f"With init_parser: {t1}s\nWith Pipeline: {t2}s")
 
