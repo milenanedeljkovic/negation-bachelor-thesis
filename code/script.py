99704d3e0b100ea3cd7bf57dea07487bd38aafd3
milenanedeljkovic <80540793+milenanedeljkovic@users.noreply.github.com>
2023-02-10 12:42:25 +0100

    cuda out of memory....again....

--- code/script.py
+++ code/script.py
@@ -1,5 +1,5 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import sys
 import spacy_conll
 import spacy_stanza
@@ -225,35 +225,36 @@ def get_contextual_embeddings(path: str, tokenizer, model, device):
     return verb_embs, num_phrases, num_phrases, num_complex_phrases, num_negations, num_negations_in_dependent_clauses
 
 
-dependency_trees = sys.argv[1]  # the file with parsed phrases
+with torch.no_grad():
+    dependency_trees = sys.argv[1]  # the file with parsed phrases
 
-tokenizer = AutoTokenizer.from_pretrained("roberta-base")
-model = AutoModel.from_pretrained("roberta-base")
+    tokenizer = AutoTokenizer.from_pretrained("roberta-base")
+    model = AutoModel.from_pretrained("roberta-base")
 
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-model.to(device)
+    # device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    # model.to(device)
 
-if os.path.isfile("verb_embeddings"):
-    verb_embeddings = torch.load("verb_embeddings")
-else:
-    verb_embeddings = {}
-
-
-embeddings, num_phrases, num_complex_phrases, num_negations, num_negations_in_dependent_clauses =\
-    get_contextual_embeddings(dependency_trees, tokenizer, model, device), 0, 0, 0, 0
-
-for verb in embeddings:
-    if verb not in verb_embeddings:
-        verb_embeddings[verb] = embeddings[verb]
+    if os.path.isfile("verb_embeddings"):
+        verb_embeddings = torch.load("verb_embeddings")
     else:
-        verb_embeddings[verb] += embeddings[verb]  # this is addition of lists!
+        verb_embeddings = {}
 
-torch.save(verb_embeddings, "verb_embeddings")
 
-with open(f"{dependency_trees[:-5]}_stats.txt", "a") as file:
-    file.write(f"Number of phrases: {num_phrases}\n")
-    file.write(f"Number of complex phases: {num_complex_phrases} ({num_complex_phrases / num_phrases})\n")
-    file.write(f"Number of negated phrases: {num_negations} ({num_negations / num_phrases})\n")
-    file.write(f"Number of negations in dependent clauses: {num_negations_in_dependent_clauses} "
-               f"({num_negations_in_dependent_clauses / num_negations})")
+    embeddings, num_phrases, num_complex_phrases, num_negations, num_negations_in_dependent_clauses =\
+        get_contextual_embeddings(dependency_trees, tokenizer, model, device), 0, 0, 0, 0
+
+    for verb in embeddings:
+        if verb not in verb_embeddings:
+            verb_embeddings[verb] = embeddings[verb]
+        else:
+            verb_embeddings[verb] += embeddings[verb]  # this is addition of lists!
+
+    torch.save(verb_embeddings, "verb_embeddings")
+
+    with open(f"{dependency_trees[:-5]}_stats.txt", "a") as file:
+        file.write(f"Number of phrases: {num_phrases}\n")
+        file.write(f"Number of complex phases: {num_complex_phrases} ({num_complex_phrases / num_phrases})\n")
+        file.write(f"Number of negated phrases: {num_negations} ({num_negations / num_phrases})\n")
+        file.write(f"Number of negations in dependent clauses: {num_negations_in_dependent_clauses} "
+                   f"({num_negations_in_dependent_clauses / num_negations})")
 
