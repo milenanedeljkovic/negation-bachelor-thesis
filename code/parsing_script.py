cf6d3523bed7e7eaec923e7f1e256918bfffaebe
milenanedeljkovic <80540793+milenanedeljkovic@users.noreply.github.com>
2023-02-15 18:16:32 +0100

    incrementally parse every 10000 pages

--- code/parsing_script.py
+++ code/parsing_script.py
@@ -27,34 +27,36 @@ def txt_to_conll(text: str, nlp):
     return doc._.conll_str
 
 with torch.no_grad():
-    if len(sys.argv) != 4:
-        raise AttributeError(f"Script takes 3 arguments: the first and last index to parse and the output file.")
-    first_to_parse = int(sys.argv[1])
-    last_to_parse = int(sys.argv[2])
-    writefile = sys.argv[3]
+    # if len(sys.argv) != 4:
+    #     raise AttributeError(f"Script takes 3 arguments: the first and last index to parse and the output file.")
+    # first_to_parse = int(sys.argv[1])
+    # last_to_parse = int(sys.argv[2])
+    # writefile = sys.argv[3]
 
     dataset = load_dataset("bigscience-data/roots_en_wikipedia", use_auth_token=True)
 
-    if last_to_parse > len(dataset['train']):
-        raise ValueError(f"Cannot parse up to index {last_to_parse}, the length of the dataset is {len(dataset['train'])}.")
+    # if last_to_parse > len(dataset['train']):
+    #     raise ValueError(f"Cannot parse up to index {last_to_parse}, the length of the dataset is {len(dataset['train'])}.")
 
     nlp = spacy_conll.init_parser("en", "stanza", parser_opts={"use_gpu": True, "verbose": False}, include_headers=True)
 
-    now = datetime.now()
-
-    current_time = now.strftime("%H:%M:%S")
-    print("Parsing Start Time =", current_time)
+    current_time = datetime.now().strftime("%H:%M:%S")
+    print(f"Parsing start time: {current_time}")
 
     try:
-        for i in range(first_to_parse, last_to_parse):
-            page_text = dataset['train'][i]['text']
-            with open(writefile, "a") as file:
-                file.write(txt_to_conll(page_text, nlp))
+        for first_to_parse in range(80000, 2040001, 10000):
+            last_to_parse = first_to_parse + 10000
+            for i in range(first_to_parse, last_to_parse):
+                page_text = dataset['train'][i]['text']
+                with open(f"parsed/parsed{first_to_parse}.conll", "w") as file:
+                    file.write(txt_to_conll(page_text, nlp))
+                    current_time = datetime.now().strftime("%H:%M:%S")
+                    print(f"Pages [{first_to_parse}:{last_to_parse}] written to file at: ", current_time)
+
     except:
         raise RuntimeError("Parsing failed.")
 
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    print("Parsing End Time =", current_time)
-    print(f"Number of pages processed: {last_to_parse - first_to_parse}")
+    current_time = datetime.now().strftime("%H:%M:%S")
+    print(f"Parsing end time: {current_time}")
+    # print(f"Number of pages processed: {last_to_parse - first_to_parse}")
 
