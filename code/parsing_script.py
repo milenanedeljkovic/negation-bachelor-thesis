4f4c8a345cc0cc8da7e5f5cfdf10766312797bfd
milenanedeljkovic <80540793+milenanedeljkovic@users.noreply.github.com>
2023-02-16 17:28:52 +0100

    change gpu in parsing script

--- code/parsing_script.py
+++ code/parsing_script.py
@@ -6,7 +6,7 @@ import sys
 from datetime import datetime
 import torch
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 def txt_to_conll(text: str, nlp):
