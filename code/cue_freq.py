63668dbee8f75a618542dcb04855864198b83652
milenanedeljkovic <80540793+milenanedeljkovic@users.noreply.github.com>
2023-03-13 13:49:50 +0100

    get cue frequencies - tomporary file

---
+++ code/cue_freq.py
@@ -0,0 +1,56 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+import sys
+import spacy_conll
+import spacy_stanza
+import torch
+from transformers import AutoModel, AutoTokenizer
+import torch
+from datetime import datetime
+import conllu
+
+def neg_cue_frequency(path: str):
+    # reading the file from path
+    f = open(path)
+    dep_trees = conllu.parse_incr(f)
+
+    num_not, num_never, num_more, num_longer = 0, 0, 0, 0
+
+    for phrase in dep_trees:
+        for word in phrase:
+            if word['lemma'] in ["not", "cannot", "can't"]:
+                num_not += 1
+            elif word['lemma'] == "never":
+                num_never += 1
+            elif word['lemma'] == 'more' and word['id'] > 1 and phrase[word['id'] - 2]['lemma'] == 'no':
+                num_more += 1
+            elif word['lemma'] == 'longer' and word['id'] > 1 and phrase[word['id'] - 2]['lemma'] == 'no'
+                num_longer += 1
+
+
+with torch.no_grad():
+    lower, upper = int(sys.argv[1]), int(sys.argv[2])
+
+    tot_not, tot_never, tot_more, tot_longer = 0, 0, 0, 0
+
+    for first_page in range(lower, upper, 10000):
+        dependency_trees = f"parsed/parsed{first_page}.conll"  # the file with parsed phrases
+
+        if not os.path.isfile(dependency_trees):
+            print(f"Not a file: {dependency_trees}")
+
+        nn, nnv, nm, nl = neg_cue_frequency(dependency_trees)
+
+        tot_not += nn
+        tot_never += nnv
+        tot_more += nm
+        tot_longer += nl
+
+    total = tot_not + tot_never + tot_more + tot_longer
+    with open("cue-freq.txt", "w") as file:
+        file.write(f"not: {tot_not} ({tot_not / total})\n")
+        file.write(f"not: {tot_never} ({tot_never / total})\n")
+        file.write(f"not: {tot_more} ({tot_more / total})\n")
+        file.write(f"not: {tot_longer} ({tot_longer / total})\n")
+
+
